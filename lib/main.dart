aedffb820d278a46fd4711ff42764e6bfa68b67c
vatiza <rakibur.mac@gmail.com>
2023-10-08 03:12:51 +0600

    update appbar title center

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,69 +10,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Class 3'),
+          centerTitle: true,
+        ),
         body: Center(
-          child: Stack(
-            children: [
-              Positioned(
-                top: 100,
-                left: 40,
-                width: 150,
-                height: 150,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(
-                      10,
-                    ),
-                  ),
-                  padding: EdgeInsets.only(top: 7, left: 15),
-                  child: Text(
-                    'Red',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              //2nd box
-              Positioned(
-                top: 130,
-                left: 60,
-                width: 150,
-                height: 150,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.purple,
-                    borderRadius: BorderRadius.circular(
-                      10,
-                    ),
-                  ),
-                  padding: EdgeInsets.only(top: 5, left: 20),
-                  child: Text(
-                    'Purple',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              // 3rd box
-              Positioned(
-                top: 160,
-                left: 80,
-                width: 150,
-                height: 150,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: BorderRadius.circular(
-                      10,
-                    ),
-                  ),
-                  padding: EdgeInsets.only(top: 10, left: 10),
-                  child: Text(
-                    'Yellow',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ],
+          child: Text(
+            'Class 3 Class 4',
+            style: GoogleFonts.actor(
+              textStyle: TextStyle(
+                  backgroundColor: Colors.amber,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
           ),
         ),
       ),
