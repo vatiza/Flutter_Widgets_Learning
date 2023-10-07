aa82888e81ed96c3139f3633d68521c35a244ce9
vatiza <rakibur.mac@gmail.com>
2023-10-08 02:59:36 +0600

    stack positioned

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,76 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          margin: const EdgeInsets.only(top: 150, left: 20),
-          width: 200,
-          height: 80,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(50)),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black, //New
-                  blurRadius: 25.0,
-                  offset: Offset(00, -10)) //top
+        body: Center(
+          child: Stack(
+            children: [
+              Positioned(
+                top: 100,
+                left: 40,
+                width: 150,
+                height: 150,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ),
+                  ),
+                  padding: EdgeInsets.only(top: 7, left: 15),
+                  child: Text(
+                    'Red',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              //2nd box
+              Positioned(
+                top: 130,
+                left: 60,
+                width: 150,
+                height: 150,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ),
+                  ),
+                  padding: EdgeInsets.only(top: 5, left: 20),
+                  child: Text(
+                    'Purple',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              // 3rd box
+              Positioned(
+                top: 160,
+                left: 80,
+                width: 150,
+                height: 150,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ),
+                  ),
+                  padding: EdgeInsets.only(top: 10, left: 10),
+                  child: Text(
+                    'Yellow',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
             ],
-          ),
-          child: Container(
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(50)),
-                color: Colors.red),
           ),
         ),
       ),
