fc04574f296a295b169714e8247dfa3afcdfd9bb
vatiza <rakibur.mac@gmail.com>
2023-10-08 00:56:53 +0600

    show text center

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Class 3 Class 4',
+            style: GoogleFonts.actor(
+              textStyle: TextStyle(
+                  backgroundColor: Colors.amber,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
