e44226b4c836ed15e77f7e165aadc5de0811774d
vatiza <rakibur.mac@gmail.com>
2023-10-14 18:50:46 +0600

    add text

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -11,15 +11,50 @@ class MainPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Container(width: 300,height: 150,
-        decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(15),
-          boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5),
-          spreadRadius: 3,
-          blurRadius: 10,
-          )]
-        ),),
-        
+        child: Container(
+          width: 300,
+          height: 150,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 3,
+                  blurRadius: 10,
+                ),
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10, left: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 160, top: 20),
+                      child: Text(
+                        'Dr. ABC',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 100),
+                      child: Text(
+                        'Dhanmondi,Dhaka 1209',
+                        style: TextStyle(),
+                      ),
+                    )
+                  ],
+                ),
+                Image.asset(
+                  "assets/img/male.png",
+                  height: 50,
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
