af64a35006cbeb41c3aea94670c6e270c30df7ee
vatiza <rakibur.mac@gmail.com>
2023-10-15 01:31:58 +0600

    done

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -16,72 +16,88 @@ class MainPage extends StatelessWidget {
           const Padding(padding: EdgeInsets.only(top: 20)),
           Center(
             child: Container(
-                width: 300,
-                height: 150,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 3,
-                        blurRadius: 10,
-                      ),
-                    ]),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, left: 10),
-                      child: Image.asset(
-                        "assets/img/male.png",
-                        height: 70,
-                      ),
+              width: 300,
+              height: 150,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 10,
                     ),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 20, left: 20),
-                      child: Column(
-                        children: [
-                          Text(
-                            'Brittany Andreoli, MD',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black),
-                          ),
-                          Text(
-                            'Otology - Gastroenterology',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w300, fontSize: 12),
-                          ),
-                          Row(
+                  ]),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20, left: 10),
+                    child: Image.asset(
+                      "assets/img/male.png",
+                      height: 70,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20, left: 20),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Brittany Andreoli, MD',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.black),
+                        ),
+                        Text(
+                          'Otology - Gastroenterology',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300, fontSize: 12),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.access_alarm,
+                            ),
+                            Text(
+                              '08:00 AM - 01:00 PM',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 50),
+                          child: Row(
                             children: [
-                              Icon(
-                                Icons.access_alarm,
-                              ),
-                              Text(
-                                '08:00 AM - 01:00 PM',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              )
+                              Card(
+                                  color: Colors.amber,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(12)),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10.0),
+                                    child: Text("Appointment"),
+                                  ))
                             ],
                           ),
-                          Padding(
-                            padding: EdgeInsets.only(right: 70, top: 20),
-                            child: Row(
-                              children: [
-                                Text(
-                                  '\$20.25',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 25),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                )),
+                        ),
+                        // Padding(
+                        //   padding: EdgeInsets.only(right: 70, top: 20),
+                        //   child: Row(
+                        //     children: [
+                        //       Text(
+                        //         '\$20.25',
+                        //         style: TextStyle(
+                        //             fontWeight: FontWeight.bold, fontSize: 25),
+                        //       ),
+                        //     ],
+                        //   ),
+                        // ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           const SizedBox(
             height: 30,
