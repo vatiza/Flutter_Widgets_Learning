76f4777f22e51f6d5e6ed19678dc6154732083e9
vatiza <rakibur.mac@gmail.com>
2023-10-14 18:25:03 +0600

    add img

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -5,6 +5,28 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Doctor Help'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          color: Colors.black,
+          elevation: 10,
+          child: SizedBox(
+            height: 150,
+            width: 260,
+            child: Image.asset(
+              'assets/img/male.png',
+              width: 10,
+              height: 10,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
