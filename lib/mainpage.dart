c091276c098cdcd98c663a5c0c5e35d09495e5fd
vatiza <rakibur.mac@gmail.com>
2023-10-15 00:28:15 +0600

    complete 2 card

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -11,72 +11,158 @@ class MainPage extends StatelessWidget {
         title: const Text('Doctor Help'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Container(
-            width: 300,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 3,
-                    blurRadius: 10,
-                  ),
-                ]),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 20, left: 10),
-                  child: Image.asset(
-                    "assets/img/male.png",
-                    height: 70,
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 20, left: 20),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Brittany Andreoli, MD',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.black),
+      body: Column(
+        children: [
+          const Padding(padding: EdgeInsets.only(top: 20)),
+          Center(
+            child: Container(
+                width: 300,
+                height: 150,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
                       ),
-                      Text(
-                        'Otology - Gastroenterology',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300, fontSize: 12),
+                    ]),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, left: 10),
+                      child: Image.asset(
+                        "assets/img/male.png",
+                        height: 70,
                       ),
-                      Row(
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 20, left: 20),
+                      child: Column(
                         children: [
-                          Icon(
-                            Icons.access_alarm,
+                          Text(
+                            'Brittany Andreoli, MD',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
                           ),
                           Text(
-                            '08:00 AM - 01:00 PM',
-                            style: TextStyle(fontWeight: FontWeight.bold),
+                            'Otology - Gastroenterology',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300, fontSize: 12),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.access_alarm,
+                              ),
+                              Text(
+                                '08:00 AM - 01:00 PM',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 70, top: 20),
+                            child: Row(
+                              children: [
+                                Text(
+                                  '\$20.25',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25),
+                                ),
+                              ],
+                            ),
                           )
                         ],
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(right: 70, top: 20),
-                        child: Row(
-                          children: [
-                            Text(
-                              '\$20.25',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 25),
-                            ),
-                          ],
+                    )
+                  ],
+                )),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+
+          //! second Card
+          Column(
+            children: [
+              Center(
+                child: Container(
+                    width: 300,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 3,
+                            blurRadius: 10,
+                          ),
+                        ]),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20, left: 10),
+                          child: Image.asset(
+                            "assets/img/female.png",
+                            height: 70,
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            )),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 20, left: 20),
+                          child: Column(
+                            children: [
+                              Text(
+                                'Brittany Andreoli, MD',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                              Text(
+                                'Otology - Gastroenterology',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w300, fontSize: 12),
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.access_alarm,
+                                  ),
+                                  Text(
+                                    '08:00 AM - 01:00 PM',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(right: 70, top: 20),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      '\$20.25',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 25),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    )),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
