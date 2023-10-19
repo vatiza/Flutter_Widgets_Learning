6ecdcdc35d9d6b959df0ae66523544cb47d95900
vatiza <rakibur.mac@gmail.com>
2023-10-19 21:14:10 +0600

    homeWork start

---
+++ android/app/src/main/kotlin/com/example/textfieldhomeworkclass7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.textfieldhomeworkclass7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/person.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PersonForm extends StatefulWidget {
+  const PersonForm({super.key});
+
+  @override
+  State<PersonForm> createState() => _PersonFormState();
+}
+
+class _PersonFormState extends State<PersonForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Input Your Information',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
+}
